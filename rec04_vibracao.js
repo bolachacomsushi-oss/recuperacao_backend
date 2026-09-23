f18c1e6fbc65a9cfb6058096e1d3af975e43e1bc
bolachacomsushi-oss <bolachacomsushi@gmail.com>
2026-09-23 14:29:41 -0300

    EXERCICIO 4

---
+++ rec04_vibracao.js
@@ -0,0 +1,13 @@
+const entrada = require(`readline-sync`)
+
+console.log("=== CLASSIFICACAO DE VIBRACAO ===")
+
+const vibracao = entrada.questionFloat("Qual o valor da vibracao? ")
+
+if (vibracao <= 3) {
+    console.log(`A vibracao esta em ${vibracao.toFixed(2)} e esta ESTAVEL`)
+} else if (vibracao <=6) {
+    console.log(`A vibracao esta em ${vibracao.toFixed(2)} e situcao e: ATENCAO`)
+} else {
+    console.log(`A vibracao esta em ${vibracao.toFixed(2)} e esta CRITICA`)
+} 
