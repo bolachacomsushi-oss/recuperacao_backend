ff0b1beb92ffec4ebd4a77c3bb0535a23ce57138
bolachacomsushi-oss <bolachacomsushi@gmail.com>
2026-09-23 14:31:58 -0300

    correcao

--- rec04_vibracao.js
+++ rec04_vibracao.js
@@ -7,7 +7,7 @@ const vibracao = entrada.questionFloat("Qual o valor da vibracao? ")
 if (vibracao <= 3) {
     console.log(`A vibracao esta em ${vibracao.toFixed(2)} e esta ESTAVEL`)
 } else if (vibracao <=6) {
-    console.log(`A vibracao esta em ${vibracao.toFixed(2)} e situcao e: ATENCAO`)
+    console.log(`A vibracao esta em ${vibracao.toFixed(2)} e exibe ATENCAO`)
 } else {
     console.log(`A vibracao esta em ${vibracao.toFixed(2)} e esta CRITICA`)
 } 
